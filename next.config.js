f650dbc97ec87ad6f5f76d892d5f9f8286cf602d
diptnc <chakrabortyd943@gmail.com>
2023-02-17 22:30:29 +0530

    sfs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+//disable eslint for this file
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  
 }
 
 module.exports = nextConfig
